9dcd60f3dad2da4415c8d2fb6dfce6d3c0e8139a
Jamsey911 <jamesalynch911@hotmail.com>
2023-04-30 22:04:00 +0000

    Add bootstarp alerts to blog

--- jims/settings.py
+++ jims/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -63,6 +64,15 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 # Redirects to home page once logged out
 LOGOUT_REDIRECT_URL = '/'
+
+# Creates on-screen alerts
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
