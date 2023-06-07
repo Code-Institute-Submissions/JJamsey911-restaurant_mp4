5b0ba4d9c43d367d9a0bdbcdf9efda8e7043266e
Jamsey911 <>
2023-06-07 01:55:15 +0000

    Add inital testcases to bookings app

--- jims/settings.py
+++ jims/settings.py
@@ -130,9 +130,9 @@ WSGI_APPLICATION = "jims.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
 
