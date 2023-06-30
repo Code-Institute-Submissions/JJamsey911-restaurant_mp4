6d28d1f5aa074cd48cc554410843872f48d76c2a
Jamsey911 <jamesalynch911@hotmail.com>
2023-06-30 13:38:18 +0000

    Add whitenoise.runserver_nostatic to installed apps in settings.py

--- jims/settings.py
+++ jims/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import os
 import dj_database_url
 
 if os.path.isfile("env.py"):
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = [
     "localhost",
 ]
 
-X_FRAME_OPTIONS = "SAMEORIGIN"
+CSRF_TRUSTED_ORIGINS = ['https://8000-jamsey911-restaurantmp4-gdb5dib3tet.ws-eu101.gitpod.io', 'https://www.heroku.com/']
 
 # To Prevents 500 errors during login and registration
 ACCOUNT_EMAIL_VERIFICATION = "none"
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     "bookings",
     "blog",
     "contact",
+    "whitenoise.runserver_nostatic",
 ]
 
 # Sets default phonenumber field format to irish
@@ -97,16 +98,15 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
 
 ROOT_URLCONF = "jims.urls"
 
@@ -178,13 +178,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-MEDIA_URL = "/media/"
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+MEDIA_URL = "/media/"
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
