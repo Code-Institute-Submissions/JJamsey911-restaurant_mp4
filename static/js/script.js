11959e77ff8a6e7cbeda41d61daec4274d3f6a9d
Jamsey911 <jamesalynch911@hotmail.com>
2023-07-06 11:50:15 +0000

    Final deployment for review

--- static/js/script.js
+++ static/js/script.js
@@ -4,5 +4,5 @@ $(document).ready(function () {
         let messages = document.getElementById("msg");
         let alert = new bootstrap.Alert(messages);
         alert.close();
-    }, 5000);
+    }, 3000);
 });
