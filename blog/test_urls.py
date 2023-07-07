679d4068041d9c37ad37c7a3c4d466dea0aa209d
Jamsey911 <jamesalynch911@hotmail.com>
2023-07-07 10:10:52 +0000

    Remove white space in blog test urls

--- blog/test_urls.py
+++ blog/test_urls.py
@@ -1,4 +1,4 @@
-# Test Booking URLS 
+# Test Booking URLS
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
@@ -17,7 +17,6 @@ class TestBlogUrls(SimpleTestCase):
     This class is for testing the bookings app
     urls
     """
-
     def test_PostList_resolved(self):
         url = reverse("blog")
         self.assertEquals(resolve(url).func.view_class, PostList)
